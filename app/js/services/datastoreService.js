6b6b32be34414014ab53ced8c7175698b3a01ce3
Wose <Jacksonteather@hotmail.com>
2017-04-21 22:12:30 -0400

    ⚡️ Updated section-1 checklist for 2017

--- app/js/services/datastoreService.js
+++ app/js/services/datastoreService.js
@@ -18,153 +18,103 @@ function DatastoreService() {
       show: false,
       month: 'May',
       points: [{ 
-          text: 'Take a <a target="_blank" href="http://uoit.ca/main/future-students/undergraduate/campus-tours-and-events/index.php?utm_source=redirect&utm_medium=web&utm_campaign=tours">campus tour</a> or check out our <a target="_blank" href="http://uoit.ca/virtualtour/">virtual tour</a>.',
-          highlight: {
-            active: false,
-            locations: [{
-              code: 'virtualtour',
-              name: 'UOIT Virtual Tour',
-              desc: '<p><a target="_blank" href="http://uoit.ca/virtualtour/">Explore our campus from home with a virtual tour! <span class="ion-android-open"></span></a></p>',
-              coords: [43.945127,-78.89683]
-            }],
-            transformClass: 'rotateX',
-            icon: 'icon icon_map',
-            marker: 'images/icons/marker_tour.svg',
-            zoom: 18
-          },
+          text: 'Take a <a target="_blank" href="http://uoit.ca/main/future-students/undergraduate/campus-tours-and-events/index.php?utm_source=nextsteps&utm_medium=web&utm_campaign=tours">campus tour</a> or check out our <a target="_blank" href="http://uoit.ca/virtualtour?utm_source=nextsteps&utm_medium=web&utm_campaign=virtualtour">virtual tour</a>.',
           complete: false,
           show: false
         },{ 
-          text: '<a target="_blank" href="http://blog.uoit.ca/hunters-road-trip-to-welcome-our-incoming-class">See Hunter the Ridgeback’s welcome</a> for our incoming class.',
+          text: '<a target="_blank" href="https://www.youtube.com/watch?v=LnehX28dP2A">See Hunter the Ridgeback’s welcome</a> for our incoming class.',
           complete: false,
-          show: false
+          show: false,
+          level: 101
       }]
     }, {
       show: false,
       month: 'June',
       points: [{ 
-          text: '<a target="_blank" href="http://uoit.ca/main/future-students/welcome/accepting-offer-101.php">Accept your offer of admission</a> by Wednesday, June 1.',
+          text: '<a target="_blank" href="http://uoit.ca/main/future-students/welcome/accepting-offer-101.php?utm_source=nextsteps&utm_medium=web&utm_campaign=accept">Accept your offer of admission</a> by Thursday, June 1.',
           complete: false,
           show: false,
           level: 101
         },{ 
-          text: '<a target="_blank" href="http://uoit.ca/main/current-students/campus-services/housing-options/on-campus.php">Apply to residence</a> by Wednesday, June 1.',
-          highlight: {
-            active: false,
-            locations: [{ 
-              name: 'Simcoe Village Residence',
-              code: 'ressim',
-              desc: '<p>Our Simcoe Village (Central Hall, North Hall, South Hall) residence offers a number of different fully furnished suites.</p>',
-              coords: [43.944225,-78.892008],
-              show: false
-            },{
-              name: 'South Village Residence',
-              code: 'ressv',
-              desc: '<p>Our South Village residence offers suites with two private bedrooms, a three-piece bathroom and kitchenette.</p>',
-              coords: [43.941908,-78.897004],
-              show: false
-            }],
-            icon: 'icon icon_home',
-            marker: 'images/icons/marker_home.svg',
-            zoom: 16
-          },
+          text: '<a target="_blank" href="http://uoit.ca/main/current-students/campus-services/housing-options/on-campus.php?utm_source=nextsteps&utm_medium=web&utm_campaign=residence">Apply to residence</a> by Thursday, June 1.',
           complete: false,
           show: false
         },{ 
-          text: 'Attend <a target="_blank" href="https://uoit.ca/forms/online/view.php?id=40504">Financing Your Education</a> information night on Tuesday, June 7 or Monday, June 13.',
-          highlight: {
-            active: false,
-            locations: [{ 
-              name: 'Science Building (UA)',
-              code: 'ua',
-              desc: '<p>Our Student Awards and Financial Aid (SAFA) office invites you to <a target="_blank" href="https://uoit.ca/forms/online/view.php?id=40504">Financing Your Education</a>, a presentation about government student assistance programs and other options of financial assistance to pay for post-secondary study.</p>',
-              coords: [43.944584, -78.896433],
-              show: false
-            }],
-            icon: 'icon icon_piggybank',
-            marker: 'images/icons/marker_finance.svg',
-            zoom: 18
-          },
+          text: 'Submit your final post-secondary transcripts and course syllabi by Thursday, June 1 to ensure your transfer credit evaluation is complete before the beginning of the Fall semester. For details on what is required, visit your <a target="_blank" href="https://uoit.ca/mycampus/">applicant portal</a>.',
+          complete: false,
+          show: false,
+          level: 105
+        },{ 
+          text: 'Attend <a target="_blank" href="https://uoit.ca/forms/online/view.php?id=40504">Financing Your Education</a> information night on Wednesday, June 14 or Tuesday, June 20.',
           complete: false,
           show: false
         },{ 
-          text: '<a target="_blank" href="http://uoit.ca/main/future-students/welcome/paying-your-confirmation-deposit.php">Pay your $500 non-refundable tuition deposit</a> by Wednesday, June 15.',
+          text: '<a target="_blank" href="https://uoit.ca/future-students/welcome/accept-your-offer/pay-your-deposit-ouac.php?utm_source=nextsteps&utm_medium=web&utm_campaign=deposit">Pay your $500 non-refundable tuition deposit</a> by Thursday, June 15.',
           complete: false,
           show: false
         },{ 
-          text: 'Register for courses. You will receive an email from the Registrar’s office with your registration window and instructions.',
+          text: '<a target="_blank" href="http://uoit.ca/future-students/welcome/101-applicant/course-registration/index.php?utm_source=nextsteps&utm_medium=web&utm_campaign=register">Register for courses</a>. You will receive an email from the Registrar’s office with your registration window and instructions.',
           complete: false,
           show: false
+				},{ 
+          text: 'If you are upgrading over the summer, submit an upgrading form by Friday, June 30.',
+          complete: false,
+          show: false,
+          level: 101
+      }]
+    }, {
+      show: false,
+      month: 'July',
+      points: [{ 
+        text: 'Register for your <a target="_blank" href="https://uoit.ca/mycampus/">laptop pick-up</a> if you are in one of the following:<ul class="block-list"><li>All programs in the Faculty of Energy Systems and Nuclear Science</li><li>All programs in the Faculty of Engineering and Applied Science</li><li>Game Development and Entrepreneurship program</li></ul>',
+        complete: false,
+        show: false
+      },{ 
+        text: '<a target="_blank" href="http://studentlife.uoit.ca/student-experience-centre/orientation/ibegin.php?utm_source=nextsteps&utm_medium=web&utm_campaign=ibegin">Attend iBegin</a> on your faculty-specific date',
+        complete: false,
+        show: false
       }]
     }, {
       show: false,
       month: 'August',
       points: [{ 
-          text: 'Order your textbooks through the <a target="_blank" href="https://uoit.ca/faculty_staff/campus_services/facilities/campus_services/campus-bookstore.php">Campus Bookstore</a>.',
-          highlight: {
-            active: false,
-            locations: [{
-              name: 'North Oshawa campus location', 
-              code: 'bs',
-              desc: '<p>The following faculties are located at our North Oshawa campus location:</p><ul>'+
-                '<li>Business and Information Technology</li>'+
-                '<li>Energy Systems and Nuclear Science</li>'+
-                '<li>Engineering and Applied Science</li>'+
-                '<li>Health Sciences</li>'+
-                '<li>Science</li></ul>',
-              coords: [43.943905,-78.896586],
-              show: false
-            },{ 
-              name: 'Downtown Oshawa campus location',
-              code: 's61',
-              desc: '<p>Our Faculty of Education and Faculty of Social Science and Humanities are located at our downtown Oshawa campus location.</p>',
-              coords: [43.897653, -78.858074],
-              show: false
-            }],
-            icon: 'icon icon_books',
-            marker: 'images/icons/marker_books.svg',
-            zoom: 12
-          },
-          complete: false,
-          show: false
-        },{ 
-          text: 'Pick up your <a target="_blank" href="http://uoit.ca/main/current-students/campus-services/safety-security/services/campus-id/how-to-get-a-campus-id-card.php">student ID card</a>.',
-          complete: false,
-          show: false
-        },{ 
-          text: '<a target="_blank" href="http://uoit.ca/main/current-students/academics/finances/tuition-and-fees/payment-options/index.php">Pay your first instalment of fees</a> by Friday, August 12. You’ll find a complete breakdown of your fees in your <a target="_blank" href="http://uoit.ca/mycampus/">MyCampus account</a>.',
-          highlight: {
-            active: false,
-            locations: [{
-              name: 'U5 Building',
-              code: 'u5', 
-              desc: '<p>The Registrar’s office and Student Life office are located in U5.</p>', 
-              coords: [43.946114,-78.896414],
-              show: false
-            },{
-              name: '61 Charles Street',
-              code: 'charles',  
-              desc: '<p>The former Alger Press Building was renovated and refurbished in 2010. 61 Charles features lecture halls, study areas, student services and the Social Science Library.</p>', 
-              coords: [43.897411,-78.857884],
-              show: false
-            }],
-            icon: 'icon icon_piggybank',
-            marker: 'images/icons/marker_finance.svg',
-            zoom: 13
-          },
-          complete: false,
-          show: false
-      }, {
-        text: 'Submit any outstanding documentation (including upgrading marks) by August 12. Please note: Summer upgrading marks (completed after June 30) are not accepted for competitive programs (Nursing and Medical Laboratory Science).',
+        text: 'Order your textbooks through the <a target="_blank" href="https://uoit.bookware3000.ca/">Campus Bookstore</a>.',
         complete: false,
-        show: false,
-        level: 105
+        show: false
+      },{ 
+        text: 'Pick up your <a target="_blank" href="https://uoit.ca/current-students/campus-services/safety-security/services/campus-id/how-to-get-a-campus-id-card.php?utm_source=nextsteps&utm_medium=web&utm_campaign=campusid">student ID card</a>.',
+        complete: false,
+        show: false
+      },{
+      	text: 'Submit any outstanding documentation (including upgrading marks) by Friday, August 11.<br/><small><strong>Please note:</strong> Summer upgrading marks (completed after June 30) will not be accepted for competitive programs such as Nursing and Medical Laboratory Science.</small>',
+      	complete: false,
+      	show: false,
+      	level: 105
+      },{
+      	text: 'Submit final marks for upgrading courses by Friday, August 11.',
+      	complete: false,
+      	show: false,
+      	level: 101
+      },{ 
+        text: '<a target="_blank" href="https://uoit.ca/current-students/academics/finances/tuition-and-fees/payment-options/index.php?utm_source=nextsteps&utm_medium=web&utm_campaign=fees">Pay your first instalment of fees</a> by Friday, August 11. You’ll find a complete breakdown of your fees in your <a target="_blank" href="http://uoit.ca/mycampus/">MyCampus account</a>.',
+        complete: false,
+        show: false
+      },{ 
+        text: '<a target="_blank" href="http://studentlife.uoit.ca/student-learning/smart-start.php?utm_source=nextsteps&utm_medium=web&utm_campaign=smartstart">Attend Smart Start</a> to build a strong academic foundation for university.',
+        complete: false,
+        show: false
       }]
     }, {
       show: false,
       month: 'September',
-      points: [{ 
-          text: 'Classes begin Thursday, September 8.'
+      points: [{
+      	text: '<a target="_blank" href="http://studentlife.uoit.ca/student-experience-centre/orientation/options.php?utm_source=nextsteps&utm_medium=web&utm_campaign=orientation">Attend September Orientation</a> on Tuesday, September 5 and Wednesday, September 6.',
+        complete: false,
+        show: false
+      },{ 
+        text: 'Classes begin Thursday, September 7.',
+        complete: false,
+        show: false
       }]
     }],
     // SECTION 3 MONTH CHECKLIST
