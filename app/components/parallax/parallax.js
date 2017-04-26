ee67716e9909cb984637d444dff711628ce9172a
Wose <Jacksonteather@hotmail.com>
2017-04-26 12:12:34 -0400

    🚿 Updated parallax module name for consistency

--- app/components/parallax/parallax.js
+++ app/components/parallax/parallax.js
@@ -3,7 +3,7 @@ import angular from 'angular';
 import { ParallaxDirective } from './parallax.directive';
 import { ParallaxBgDirective } from './parallax-bg.directive';
 
-export const ParallaxModule = angular.module('bgParallax', [])
+export const ParallaxModule = angular.module('parallax', [])
 	.directive('parallax', ParallaxDirective)
 	.directive('parallaxBackground', ParallaxBgDirective)
 	.name;
